fcb122929bf934d4ffe90be492a04d7e2f754e3e
pf-adam-burke <adam.burke@peoplefluent.com>
2015-01-21 15:56:36 -0500

    Run 3 samples

---
+++ src/RunMongoMigrations/MigrationScripts/RenameCustomerProductsToNavigationItems.cs
@@ -0,0 +1,22 @@
+﻿namespace RunMongoMigrations.MigrationScripts
+{
+    using MongoDB.Bson;
+
+    using MongoMigrations;
+
+    [Experimental]
+    class RenameCustomerProductsToNavigationItems : MigrationScript
+    {
+        public RenameCustomerProductsToNavigationItems()
+            : base("0.0.0")
+        {
+            Description = "This script will change the name of the CustomerProducts collection to NavigationItems";
+        }
+
+        public override void Update()
+        {
+            Database.Eval(new BsonJavaScript("db.CustomerProduct.renameCollection('NavigationItem');"));
+        }
+
+    }
+}
